7df0f75de1853ee0214982eace744b422576c512
Sean Haughey <sdhaughey@gmail.com>
2016-04-27 17:29:55 -0500

    created submit form and action

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,10 +1,11 @@
 var React = require('react');
 var AppActions = require('../actions/AppActions');
 var AppStore = require('../stores/AppStore');
+var AddForm = require('./AddForm.js')
 
 function getAppState(){
 	return {
-
+		contacts: AppStore.getContacts()
 	}
 };
 
@@ -22,9 +23,10 @@ var App = React.createClass({
 	},
 
 	render: function(){
+		console.log(this.state.contacts)
 		return (
 			<div>
-				MY APP
+				<AddForm />
 			</div>
 		);
 	},
--- src/js/stores/AppStore.js
+++ src/js/stores/AppStore.js
@@ -6,9 +6,15 @@ var AppAPI = require('../utils/appAPI.js');
 
 var CHANGE_EVENT = 'change';
 
-var _items = [];
+var _contacts = [];
 
 var AppStore = assign({}, EventEmitter.prototype, {
+	getContacts: function(){
+		return _contacts;
+	},
+	saveContact: function(contact){
+		_contacts.push(contact);
+	},
 	emitChange: function(){
 		this.emit(CHANGE_EVENT);
 	},
@@ -24,7 +30,15 @@ AppDispatcher.register(function(payload){
 	var action = payload.action;
 
 	switch(action.actionType){
+		case AppConstants.SAVE_CONTACT:
+		console.log('saving contact')
 
+		// Store Save
+		AppStore.saveContact(action.contact);
+
+		// Emit Change
+		AppStore.emit(CHANGE_EVENT);
+		break;
 	}
 
 	return true;
